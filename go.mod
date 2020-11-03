df15f47ff7527f2646160b7151bf7aa46e2a6cbe
Southclaws <accounts@southcla.ws>
2020-11-03 20:34:28 +0000

    restructured a bit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Southclaws/trillogy-chaos-discord-votes
 go 1.14
 
 require (
+	github.com/Southclaws/go-multicloser v1.0.0
 	github.com/bwmarrin/discordgo v0.22.0
 	github.com/joho/godotenv v1.3.0
 	github.com/natefinch/npipe v0.0.0-20160621034901-c1b8fa8bdcce
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/Southclaws/go-multicloser"
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/natefinch/npipe"
@@ -21,157 +22,172 @@ func main() {
 	}
 }
 
+type bot struct {
+	dg              *discordgo.Session
+	pipe            *npipe.PipeConn
+	channel         string
+	votemsg         *discordgo.Message
+	effects         []Effect
+	delReactHandler func()
+	options         [3]Effect
+	counts          map[string]int // map from username to vote count
+}
+
 func run() error {
-	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
-	if err != nil {
-		return err
-	}
-	channel := os.Getenv("DISCORD_CHANNEL")
-	fmt.Println("using channel:", channel)
+	b := bot{}
 
-	if err := dg.Open(); err != nil {
-		return err
-	}
+	b.channel = os.Getenv("DISCORD_CHANNEL")
+	fmt.Println("using channel:", b.channel)
 
-	ln, err := npipe.Dial(PIPE)
-	if err != nil {
-		panic(err)
+	mc := multicloser.MultiErrorMultiCloser{}
+	b.dg = mc.Add(discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))).(*discordgo.Session)
+	b.pipe = mc.Add(npipe.Dial(PIPE)).(*npipe.PipeConn)
+	defer mc.Close()
+
+	// connect to discord
+	if err := b.dg.Open(); err != nil {
+		return err
 	}
 	fmt.Println("Connected!")
 
-	effects := Effects()
+	b.effects = Effects()
 
-	var discordMessage *discordgo.Message
-	defer func() {
-		if discordMessage != nil {
-			dg.ChannelMessageDelete(channel, discordMessage.ID) // delete on shutdown
-		}
-	}()
-	var remoteReactHandler func()
-	var options [3]Effect
-	var counts map[string]int // map from username to vote count
+	defer b.deleteVoteMessage()
 
 	last := time.Now()
 	tc := time.NewTicker(time.Millisecond * 100)
-	for range tc.C {
-		message := fmt.Sprintf("time:%d,%d:-1:N/A:N/A:0", int64(COOLDOWN.Milliseconds())-time.Since(last).Milliseconds(), COOLDOWN.Milliseconds())
+	for {
+		select {
+		// TODO: sync votes
+		case <-tc.C:
+			b.write(fmt.Sprintf("time:%d,%d,%s:", int64(COOLDOWN.Milliseconds())-time.Since(last).Milliseconds(), COOLDOWN.Milliseconds() /*mode:*/, ""), false)
 
-		_, err := ln.Write([]byte(message))
-		if err != nil {
-			return err
-		}
-		// fmt.Println("writing", message, n, err)
-
-		if time.Since(last) > COOLDOWN {
-			var title string
-			if discordMessage != nil && len(counts) > 0 {
-				list := []int{0, 0, 0}
-				for _, choice := range counts {
-					list[choice]++
-				}
-
-				winner := 0
-				for i := range list {
-					if i == 0 || list[i] > list[winner] {
-						winner = i
-					}
-				}
-				fmt.Println("WINNER!", winner, options[winner].Name())
-
-				message := fmt.Sprintf(
-					"votes:%s;%d;;%s;%d;;%s;%d;;%d",
-					options[0].ID(), list[0],
-					options[1].ID(), list[1],
-					options[2].ID(), list[2],
-					winner,
-				)
-				fmt.Println("writing", message)
-				_, err := ln.Write([]byte(message))
-				if err != nil {
-					return err
-				}
-
-				effect := options[winner]
-				message = fmt.Sprintf("%s:N/A:0", EffectToMessage(effect, COOLDOWN))
-				fmt.Println("writing", message)
-				_, err = ln.Write([]byte(message))
-				if err != nil {
-					return err
-				}
-
-				dg.ChannelMessageDelete(channel, discordMessage.ID)
-
-				title = fmt.Sprintf("Winner: %s (%d votes)\n\nVote for the next effect!", options[winner].Name(), list[winner])
-			} else {
-				idx := rand.Intn(len(effects))
-				effect := effects[idx]
-				message := fmt.Sprintf("%s:N/A:0", EffectToMessage(effect, COOLDOWN))
-
-				fmt.Println("writing", message)
-				_, err := ln.Write([]byte(message))
-				if err != nil {
-					return err
-				}
-
-				title = "Vote for the next effect!"
+			if time.Since(last) < COOLDOWN {
+				continue
 			}
 
-			options[0] = effects[rand.Intn(len(effects))]
-			options[1] = effects[rand.Intn(len(effects))]
-			options[2] = effects[rand.Intn(len(effects))]
-			counts = make(map[string]int)
-
-			discordMessage, err = dg.ChannelMessageSendEmbed(channel, &discordgo.MessageEmbed{
-				Type:  discordgo.EmbedTypeRich,
-				Title: title,
-				// Description: "React to this message to vote for the next effect!",
-				Fields: []*discordgo.MessageEmbedField{
-					{Name: "A", Value: options[0].Name(), Inline: true},
-					{Name: "B", Value: options[1].Name(), Inline: true},
-					{Name: "C", Value: options[2].Name(), Inline: true},
-				},
-			})
-			if err != nil {
-				return errors.Wrap(err, "failed to send embed")
+			if err := b.handleEffect(); err != nil {
+				fmt.Println("failed to handle effect:", err)
 			}
-			if err := dg.MessageReactionAdd(channel, discordMessage.ID, "🇦"); err != nil {
-				return err
-			}
-			if err := dg.MessageReactionAdd(channel, discordMessage.ID, "🇧"); err != nil {
-				return err
-			}
-			if err := dg.MessageReactionAdd(channel, discordMessage.ID, "🇨"); err != nil {
-				return err
-			}
-
-			if remoteReactHandler != nil {
-				remoteReactHandler()
-			}
-			remoteReactHandler = dg.AddHandler(func(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
-				if e.MessageID != discordMessage.ID {
-					return
-				}
-
-				if _, ok := counts[e.UserID]; ok && e.UserID != discordMessage.Author.ID {
-					// user has already voted
-					dg.MessageReactionRemove(channel, discordMessage.ID, e.MessageReaction.Emoji.Name, e.UserID)
-					return
-				}
-
-				switch e.MessageReaction.Emoji.Name {
-				case "🇦":
-					counts[e.UserID] = 0
-				case "🇧":
-					counts[e.UserID] = 1
-				case "🇨":
-					counts[e.UserID] = 2
-				}
-			})
 
 			last = time.Now()
 		}
 	}
+}
+
+func (b *bot) handleEffect() (err error) {
+	var title string
+	if b.votemsg != nil && len(b.counts) > 0 {
+		list := []int{0, 0, 0}
+		for _, choice := range b.counts {
+			list[choice]++
+		}
+
+		winner := 0
+		for i := range list {
+			if i == 0 || list[i] > list[winner] {
+				winner = i
+			}
+		}
+		fmt.Println("[votes] Winner:", b.options[winner].Name())
+
+		b.write(fmt.Sprintf(
+			"votes:%s;%d;;%s;%d;;%s;%d;;%d",
+			b.options[0].ID(), list[0],
+			b.options[1].ID(), list[1],
+			b.options[2].ID(), list[2],
+			winner,
+		), true)
+
+		effect := b.options[winner]
+		b.write(fmt.Sprintf("%s:N/A:0", EffectToMessage(effect, COOLDOWN)), true)
+
+		b.dg.ChannelMessageDelete(b.channel, b.votemsg.ID)
+
+		title = fmt.Sprintf("Winner: %s (%d votes)\n\nVote for the next effect!", b.options[winner].Name(), list[winner])
+	} else {
+		idx := rand.Intn(len(b.effects))
+		effect := b.effects[idx]
+		message := fmt.Sprintf("%s:N/A:0", EffectToMessage(effect, COOLDOWN))
+
+		fmt.Println("writing", message)
+		_, err = b.pipe.Write([]byte(message))
+		if err != nil {
+			return err
+		}
+
+		title = "Vote for the next effect!"
+	}
+
+	b.options[0] = b.effects[rand.Intn(len(b.effects))]
+	b.options[1] = b.effects[rand.Intn(len(b.effects))]
+	b.options[2] = b.effects[rand.Intn(len(b.effects))]
+	b.counts = make(map[string]int)
+
+	b.votemsg, err = b.dg.ChannelMessageSendEmbed(b.channel, &discordgo.MessageEmbed{
+		Type:  discordgo.EmbedTypeRich,
+		Title: title,
+		// Description: "React to this message to vote for the next effect!",
+		Fields: []*discordgo.MessageEmbedField{
+			{Name: "A", Value: b.options[0].Name(), Inline: true},
+			{Name: "B", Value: b.options[1].Name(), Inline: true},
+			{Name: "C", Value: b.options[2].Name(), Inline: true},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to send embed")
+	}
+	if err := b.dg.MessageReactionAdd(b.channel, b.votemsg.ID, "🇦"); err != nil {
+		return err
+	}
+	if err := b.dg.MessageReactionAdd(b.channel, b.votemsg.ID, "🇧"); err != nil {
+		return err
+	}
+	if err := b.dg.MessageReactionAdd(b.channel, b.votemsg.ID, "🇨"); err != nil {
+		return err
+	}
+
+	if b.delReactHandler != nil {
+		b.delReactHandler()
+	}
+	b.delReactHandler = b.dg.AddHandler(func(s *discordgo.Session, e *discordgo.MessageReactionAdd) {
+		if e.MessageID != b.votemsg.ID {
+			return
+		}
+
+		if _, ok := b.counts[e.UserID]; ok && e.UserID != b.votemsg.Author.ID {
+			// user has already voted
+			b.dg.MessageReactionRemove(b.channel, b.votemsg.ID, e.MessageReaction.Emoji.Name, e.UserID)
+			return
+		}
+
+		switch e.MessageReaction.Emoji.Name {
+		case "🇦":
+			b.counts[e.UserID] = 0
+		case "🇧":
+			b.counts[e.UserID] = 1
+		case "🇨":
+			b.counts[e.UserID] = 2
+		}
+	})
+
 	return nil
+}
+
+func (b *bot) deleteVoteMessage() {
+	if b.votemsg != nil {
+		b.dg.ChannelMessageDelete(b.channel, b.votemsg.ID)
+	}
+}
+
+func (b *bot) write(message string, print bool) {
+	if print {
+		fmt.Println("[pipe] writing: '%s'", message)
+	}
+	_, err := b.pipe.Write([]byte(message))
+	if err != nil {
+		panic(err)
+	}
 }
 
 // func main() {
@@ -181,7 +197,7 @@ func run() error {
 // 		panic(err)
 // 	}
 // 	for {
-// 		conn, err := ln.Accept()
+// 		conn, err := b.pipe.Accept()
 // 		if err != nil {
 // 			panic(err)
 // 		}
